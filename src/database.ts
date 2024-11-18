a8fba4e2a95541d38c770df17af858f383b3206e
PetarPetrovf <petarkrasimirov01@gmail.com>
2024-11-18 09:13:48 +0200

    commit changes

--- src/database.ts
+++ src/database.ts
@@ -1,10 +1,11 @@
 import { Sequelize } from 'sequelize';
 import { UserModel } from './user/user';
 import { UniversityModel } from './university/university';
+import { SubjectModel } from './subject/subject';  
 
 const sequelize = new Sequelize({
     dialect: 'sqlite',
-    storage: 'database.sqlite', // SQLite database file path
+    storage: 'database.sqlite', 
 });
 
 export const db = {
@@ -12,7 +13,8 @@ export const db = {
     Sequelize,
     models: {
         User: UserModel(sequelize),
-        University: UniversityModel(sequelize)
+        University: UniversityModel(sequelize),
+        Subject: SubjectModel(sequelize),  
     },
 };
 
@@ -25,3 +27,21 @@ db.models.University.hasMany(db.models.User, {
     foreignKey: 'universityId',
     as: 'users',
 });
+
+
+db.models.User.belongsToMany(db.models.Subject, {
+    through: 'user_subjects',  
+    foreignKey: 'userId',      
+    otherKey: 'subjectId',     
+    as: 'subjects',           
+});
+
+db.models.Subject.belongsToMany(db.models.User, {
+    through: 'user_subjects', 
+    foreignKey: 'subjectId',   
+    otherKey: 'userId',        
+    as: 'users',               
+});
+
+export default db;
+
